5702de471b35146927f49b84e3c31a0ec5360a2b
hectorbaez247283 <159949506+hectorbaez247283@users.noreply.github.com>
2024-04-09 06:38:05 -0700

    Intento fallido reporte

--- objetoNegocio/src/main/java/moduloReportes/DesplegarReporteBO.java
+++ objetoNegocio/src/main/java/moduloReportes/DesplegarReporteBO.java
@@ -4,7 +4,16 @@
  */
 package moduloReportes;
 
+import conexionEM.Conexion;
+import dtos.LicenciaDTO;
+import dtos.PlacaDTO;
+import dtos.TramiteDTO;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.util.Date;
+import java.util.List;
+import javax.swing.JOptionPane;
 import javax.swing.text.Document;
 
 /**
@@ -13,152 +22,65 @@ import javax.swing.text.Document;
  * @author Diego Alcantar Acosta
  */
 public class DesplegarReporteBO implements IDesplegarReporteBO{
-    
-     private IConexionDAO conexionDAO;
-    private IReporteDAO reporteDAO;
 
-    public ReporteBO(IConexionDAO conexionDAO, IReporteDAO reporteDAO) {
-        this.conexionDAO = conexionDAO;
-        this.reporteDAO = reporteDAO;
+    public DesplegarReporteBO() {
     }
-
-    @Override
+    
+     @Override
     public void generarReportePlacas(List<PlacaDTO> placas) {
-        Document doc = new Document();
-
         try {
-            PdfWriter writer = PdfWriter.getInstance(doc, new FileOutputStream("ReportePlacas.pdf"));
-            writer.setPageEvent(new PageNumberEvent());
-
-            Font tituloFont = new Font(Font.FontFamily.HELVETICA, 18, Font.BOLD);
-            Paragraph titulo = new Paragraph("Reporte de Placas", tituloFont);
-            titulo.setAlignment(Element.ALIGN_CENTER);
-
-            Date fechaActual = new Date();
-            SimpleDateFormat formatoFecha = new SimpleDateFormat("dd/MM/yyyy");
-            Paragraph fecha = new Paragraph("Fecha de generación: " + formatoFecha.format(fechaActual));
-            fecha.setAlignment(Element.ALIGN_RIGHT);
-
-            PdfPTable tabla = new PdfPTable(4);
-            tabla.addCell(createCell("Fecha", true));
-            tabla.addCell(createCell("Costo", true));
-            tabla.addCell(createCell("Trámite", true));
-            tabla.addCell(createCell("Persona", true));
-
-            for (PlacaDTO placa : placas) {
-
-                String fechaExpedicionFormateada = (placa.getFechaExpedicion() != null) ? formatoFecha.format(placa.getFechaExpedicion().getTime()) : "NoDate";
-                tabla.addCell(createCell(fechaExpedicionFormateada, false));
-
-                tabla.addCell(createCell(String.valueOf(placa.getCosto()), false));
-                tabla.addCell(createCell("Expedición de Placas", false));
-                tabla.addCell(createCell(placa.getNombrePersona(), false));
-            }
-
-            doc.open();
-            doc.add(titulo);
-            doc.add(Chunk.NEWLINE);
-            doc.add(fecha);
-            doc.add(Chunk.NEWLINE);
-            doc.add(Chunk.NEWLINE);
-            doc.add(tabla);
-            doc.close();
-
-            JOptionPane.showMessageDialog(null, "PDF exportado! Ruta: ReportePlacas.pdf", "Éxito", JOptionPane.INFORMATION_MESSAGE);
+            File templateFile = new File("ruta_del_template_de_placas.jrxml");
+            JasperCompileManager.compileReportToFile(new FileInputStream(templateFile), "PlacasReport.jasper");
+            JasperPrint print = JasperFillManager.fillReport("PlacasReport.jasper", null, new JRBeanCollectionDataSource(placas));
+            JasperExportManager.exportReportToPdfFile(print, "ReportePlacas.pdf");
+            JOptionPane.showMessageDialog(null, "Reporte de Placas generado con éxito.", "Éxito", JOptionPane.INFORMATION_MESSAGE);
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.println("Error al generar el reporte de placas.");
+            JOptionPane.showMessageDialog(null, "Error al generar el reporte de Placas.", "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
 
     @Override
     public void generarReporteLicencias(List<LicenciaDTO> licencias) {
-        Document doc = new Document();
-
         try {
-            PdfWriter writer = PdfWriter.getInstance(doc, new FileOutputStream("ReporteLicencias.pdf"));
-            writer.setPageEvent(new PageNumberEvent());
-
-            Font tituloFont = new Font(Font.FontFamily.HELVETICA, 18, Font.BOLD);
-            Paragraph titulo = new Paragraph("Reporte de Licencias", tituloFont);
-            titulo.setAlignment(Element.ALIGN_CENTER);
-
-            Date fechaActual = new Date();
-            SimpleDateFormat formatoFecha = new SimpleDateFormat("dd/MM/yyyy");
-            Paragraph fecha = new Paragraph("Fecha de generación: " + formatoFecha.format(fechaActual));
-            fecha.setAlignment(Element.ALIGN_RIGHT);
-
-            PdfPTable tabla = new PdfPTable(5);
-            tabla.addCell(createCell("Fecha", true));
-            tabla.addCell(createCell("Costo", true));
-            tabla.addCell(createCell("Trámite", true));
-            tabla.addCell(createCell("Vigencia", true));
-            tabla.addCell(createCell("Persona", true));
-
-            for (LicenciaDTO licencia : licencias) {
-
-                String fechaExpedicionFormateada = (licencia.getFechaExpedicion() != null) ? formatoFecha.format(licencia.getFechaExpedicion().getTime()) : "NoDate";
-                tabla.addCell(createCell(fechaExpedicionFormateada, false));
-
-                tabla.addCell(createCell(String.valueOf(licencia.getCosto()), false));
-                tabla.addCell(createCell("Expedición de Licencias", false));
-
-                String fechaVigenciaFormateada = (licencia.getFechaVigencia() != null) ? formatoFecha.format(licencia.getFechaVigencia().getTime()) : "NoDate";
-                tabla.addCell(createCell(fechaVigenciaFormateada, false));
-
-                tabla.addCell(createCell(licencia.getNombrePersona(), false));
-            }
-
-            doc.open();
-            doc.add(titulo);
-            doc.add(Chunk.NEWLINE);
-            doc.add(fecha);
-            doc.add(Chunk.NEWLINE);
-            doc.add(Chunk.NEWLINE);
-            doc.add(tabla);
-            doc.close();
-
-            JOptionPane.showMessageDialog(null, "PDF exportado! Ruta: ReporteLicencias.pdf", "Éxito", JOptionPane.INFORMATION_MESSAGE);
+            File templateFile = new File("ruta_del_template_de_licencias.jrxml");
+            JasperCompileManager.compileReportToFile(new FileInputStream(templateFile), "LicenciasReport.jasper");
+            JasperPrint print = JasperFillManager.fillReport("LicenciasReport.jasper", null, new JRBeanCollectionDataSource(licencias));
+            JasperExportManager.exportReportToPdfFile(print, "ReporteLicencias.pdf");
+            JOptionPane.showMessageDialog(null, "Reporte de Licencias generado con éxito.", "Éxito", JOptionPane.INFORMATION_MESSAGE);
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.println("Error al generar el reporte de licencias.");
+            JOptionPane.showMessageDialog(null, "Error al generar el reporte de Licencias.", "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
 
     @Override
     public void generarReporteTramites(List<TramiteDTO> tramites) {
-        Document doc = new Document();
-
         try {
-            PdfWriter writer = PdfWriter.getInstance(doc, new FileOutputStream("ReporteTramites.pdf"));
-            writer.setPageEvent(new PageNumberEvent());
+            File templateFile = new File("ruta_del_template_de_tramites.jrxml");
+            JasperCompileManager.compileReportToFile(new FileInputStream(templateFile), "TramitesReport.jasper");
+            JasperPrint print = JasperFillManager.fillReport("TramitesReport.jasper", null, new JRBeanCollectionDataSource(tramites));
+            JasperExportManager.exportReportToPdfFile(print, "ReporteTramites.pdf");
+            JOptionPane.showMessageDialog(null, "Reporte de Trámites generado con éxito.", "Éxito", JOptionPane.INFORMATION_MESSAGE);
+        } catch (Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Error al generar el reporte de Trámites.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
 
-            Font tituloFont = new Font(Font.FontFamily.HELVETICA, 18, Font.BOLD);
-            Paragraph titulo = new Paragraph("Reporte de Trámites", tituloFont);
-            titulo.setAlignment(Element.ALIGN_CENTER);
+    @Override
+    public List<TramiteDTO> buscarTramites(Date fechaInicio, Date fechaFin, String nombre) {
+        return null;
+    }
 
-            Date fechaActual = new Date();
-            SimpleDateFormat formatoFecha = new SimpleDateFormat("dd/MM/yyyy");
-            Paragraph fecha = new Paragraph("Fecha de generación: " + formatoFecha.format(fechaActual));
-            fecha.setAlignment(Element.ALIGN_RIGHT);
+    @Override
+    public List<LicenciaDTO> buscarLicencias(Date fechaInicio, Date fechaFin, String nombre) {
+        return null;
+    }
 
-            PdfPTable tabla = new PdfPTable(5);
-            tabla.addCell(createCell("Fecha", true));
-            tabla.addCell(createCell("Costo", true));
-            tabla.addCell(createCell("Estado", true));
-            tabla.addCell(createCell("Persona", true));
-            tabla.addCell(createCell("Trámite", true));
-
-            for (TramiteDTO tramite : tramites) {
-                String fechaExpedicionFormateada = (tramite.getFechaExpedicion() != null) ? formatoFecha.format(tramite.getFechaExpedicion().getTime()) : "NoDate";
-                tabla.addCell(createCell(fechaExpedicionFormateada, false));
-                tabla.addCell(createCell(Double.toString(tramite.getCosto()), false));
-                tabla.addCell(createCell(String.valueOf(tramite.getEstado()), false));
-                tabla.addCell(createCell(tramite.getNombrePersona(), false));
-                tabla.addCell(createCell(tramite.getTipoTramite(), false));
-            }
-
-            doc.open();
-            doc.add(titulo);
+    @Override
+    public List<PlacaDTO> buscarPlacas(Date fechaInicio, Date fechaFin, String nombre) {
+        return null;
+    }
     
 }
